c737c9dafd67da2f78bb6fd75d514ac3b56494e5
lokas <lokashafeek7755@gmail.com>
2023-01-18 10:54:27 +0200

    add ListOfOffendingWordsPage class

--- check_usernames/core/run_checker.py
+++ check_usernames/core/run_checker.py
@@ -1,5 +1,35 @@
 from datetime import timedelta, datetime
+
+import pywikibot
 from pywikibot import Site, pagegenerators, Timestamp, User
+
+
+class ListOfOffendingWordsPage:
+    def __init__(self, site):
+        self._title = ""
+        self._page = None
+        self.site = site
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        self._title = value
+
+    @property
+    def page(self):
+        return self._page
+
+    def load_page(self):
+        self._page = pywikibot.Page(self.site, self.title)
+
+    def get_page_text(self):
+        if self._page is None:
+            self.load_page()
+        text = self._page.text
+        return text
 
 
 class NewUsersChecker:
@@ -26,7 +56,14 @@ class NewUsersChecker:
                 print(f"{user} is not blocked")
 
 
-# create an object of NewUsersChecker class
-checker = NewUsersChecker()
-# call the check_blocked method
-checker.check_blocked()
+site = Site("ar", "wikipedia")
+
+list_of_offending_words_page = ListOfOffendingWordsPage(site)
+list_of_offending_words_page.title = "ويكيبيديا:إخطار الإداريين/أسماء مستخدمين للفحص/قائمة الكلمات المخالفة"
+list_of_offending_words_page.load_page()
+
+#
+# # create an object of NewUsersChecker class
+# checker = NewUsersChecker()
+# # call the check_blocked method
+# checker.check_blocked()
