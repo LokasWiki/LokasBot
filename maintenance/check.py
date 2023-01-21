54f2c8d18abd95c77edb6b70b3d8cb4a82082d40
lokas <lokashafeek7755@gmail.com>
2023-01-21 16:45:45 +0200

    add base to get list of edits

--- maintenance/check.py
+++ maintenance/check.py
@@ -2,16 +2,40 @@
  # add remove
  # add add
  # add check
+import datetime
+
 import pywikibot
+from pywikibot import pagegenerators
 
 from bots.unreviewed_article.core import UnreviewedArticle
 
-title =  "ابن حجر العسقلاني"
-site = pywikibot.Site("ar", "wikipedia")
-page = UnreviewedArticle(site)
-page.title = title
-page.load_page()
-if not page.check():
-    page.add_template()
-else:
-    page.remove_template()
+import pywikibot
+
+site = pywikibot.Site()
+# start = pywikibot.Timestamp.fromISOformat('2022-12-06T00:00:00Z')
+start = pywikibot.Timestamp.fromISOformat('2023-01-20T00:00:00Z')
+
+end = pywikibot.Timestamp.now() - datetime.timedelta(minutes=5)
+
+
+gen = pagegenerators.RecentChangesPageGenerator(site=site, start=start, end=end, namespaces=[0],reverse=True)
+
+# To remove duplicate pages from the generator
+gen = set(gen)
+
+# To remove deleted pages from the generator,
+gen = filter(lambda page: page.exists(), gen)
+
+for entry in gen:
+    print(entry.title())
+    try:
+        title = entry.title()
+        page = UnreviewedArticle(site)
+        page.title = title
+        page.load_page()
+        if not page.check():
+            page.add_template()
+        else:
+            page.remove_template()
+    except Exception as e:
+        print(f"An error occurred while processing {entry.title()}: {e}")
