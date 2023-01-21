e5167652d1f10327b7d5a1e63732706ef56336c2
lokas <lokashafeek7755@gmail.com>
2023-01-21 17:57:15 +0200

    skip Redirect Page from gen

--- maintenance/check.py
+++ maintenance/check.py
@@ -12,10 +12,12 @@ from bots.unreviewed_article.core import UnreviewedArticle
 import pywikibot
 
 site = pywikibot.Site()
-# start = pywikibot.Timestamp.fromISOformat('2022-12-06T00:00:00Z')
-start = pywikibot.Timestamp.fromISOformat('2023-01-20T00:00:00Z')
 
-end = pywikibot.Timestamp.now() - datetime.timedelta(minutes=5)
+start = pywikibot.Timestamp.fromISOformat('2022-01-10T00:00:00Z')
+end = pywikibot.Timestamp.fromISOformat('2023-01-20T00:00:00Z')
+
+
+# end = pywikibot.Timestamp.now() - datetime.timedelta(minutes=5)
 
 
 gen = pagegenerators.RecentChangesPageGenerator(site=site, start=start, end=end, namespaces=[0],reverse=True)
@@ -27,15 +29,17 @@ gen = set(gen)
 gen = filter(lambda page: page.exists(), gen)
 
 for entry in gen:
-    print(entry.title())
-    try:
-        title = entry.title()
-        page = UnreviewedArticle(site)
-        page.title = title
-        page.load_page()
-        if not page.check():
-            page.add_template()
-        else:
-            page.remove_template()
-    except Exception as e:
-        print(f"An error occurred while processing {entry.title()}: {e}")
+    page1 = pywikibot.Page(site, entry.title())
+    if not page1.isRedirectPage():
+        print(entry.title())
+        try:
+            title = entry.title()
+            page = UnreviewedArticle(site)
+            page.title = title
+            page.load_page()
+            if not page.check():
+                page.add_template()
+            else:
+                page.remove_template()
+        except Exception as e:
+            print(f"An error occurred while processing {entry.title()}: {e}")
