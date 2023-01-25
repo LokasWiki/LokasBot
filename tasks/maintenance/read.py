118d18711a6e5bbe9f1e33ac38b1d9a27a42f92a
lokas <lokashafeek7755@gmail.com>
2023-01-25 07:59:58 +0200

    add a try-except block to handle any errors that might occur when trying to execute the SQL statements

--- tasks/maintenance/read.py
+++ tasks/maintenance/read.py
@@ -1,25 +1,20 @@
 import datetime
 import itertools
 import os
-
-import pywikibot
 from pywikibot import pagegenerators
 import time
 import sqlite3
-
-from bots.unreviewed_article.core import UnreviewedArticle
-
 import pywikibot
-
 import sys
+
 
 site = pywikibot.Site()
 
-# time_before_start = int(sys.argv[1])
-time_before_start = 1000
+time_before_start = int(sys.argv[1])
 
 start = pywikibot.Timestamp.now() - datetime.timedelta(minutes=time_before_start)
 end = pywikibot.Timestamp.now()
+
 
 gen1 = pagegenerators.RecentChangesPageGenerator(site=site, start=start, end=end, namespaces=[0], reverse=True)
 gen2 = pagegenerators.LogeventsPageGenerator(logtype="review", total=3000, site=site, start=start, end=end, namespace=0,
@@ -46,19 +41,13 @@ for entry in gen:
     page1 = pywikibot.Page(site, entry.title())
     if not page1.isRedirectPage():
         time.sleep(1)
-        title = entry.title()
-        cursor.execute("SELECT * FROM pages WHERE title = ?", (title,))
-        if cursor.fetchone() is None:
-            cursor.execute("INSERT INTO pages (title, status) VALUES (?, 0)", (title,))
-        conn.commit()
-        # try:
-        #     title = entry.title()
-        #     page = UnreviewedArticle(site)
-        #     page.title = title
-        #     page.load_page()
-        #     if not page.check():
-        #         page.add_template()
-        #     else:
-        #         page.remove_template()
-        # except Exception as e:
-        #     print(f"An error occurred while processing {entry.title()}: {e}")
+        try:
+            title = entry.title()
+            cursor.execute("SELECT * FROM pages WHERE title = ?", (title,))
+            if cursor.fetchone() is None:
+                cursor.execute("INSERT INTO pages (title, status) VALUES (?, 0)", (title,))
+            conn.commit()
+        except sqlite3.Error as e:
+            print(f"An error occurred while inserting the title {title} into the database: {e}")
+
+
