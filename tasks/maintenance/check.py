c93c29cfaff66a6a8482dd409a873e6c69f90d04
lokas <lokashafeek7755@gmail.com>
2023-01-26 08:52:21 +0200

    update maintenance.db and fix some error

--- tasks/maintenance/check.py
+++ tasks/maintenance/check.py
@@ -16,19 +16,20 @@ cursor = conn.cursor()
 
 
 # Create the table with a status column
-cursor.execute('''CREATE TABLE IF NOT EXISTS pages (title TEXT PRIMARY KEY, status INTEGER, date TIMESTAMP DEFAULT CURRENT_TIMESTAMP)''')
+cursor.execute('''CREATE TABLE IF NOT EXISTS pages (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, status INTEGER, date TIMESTAMP DEFAULT CURRENT_TIMESTAMP)''')
 
 
 try:
-    cursor.execute("SELECT title FROM pages WHERE status=0 ORDER BY date ASC LIMIT 50")
+    cursor.execute("SELECT id, title FROM pages WHERE status=0 ORDER BY date ASC LIMIT 50")
     rows = cursor.fetchall()
     if rows:
         for row in rows:
             time.sleep(2)
-            title = row[0]
+            id = row[0]
+            title = row[1]
             print(title)
             try:
-                cursor.execute("UPDATE pages SET status = 1 WHERE title = ?", (title,))
+                cursor.execute("UPDATE pages SET status = 1 WHERE id = ?", (id,))
                 conn.commit()
                 page = UnreviewedArticle(site)
                 page.title = title
@@ -37,14 +38,16 @@ try:
                     page.add_template()
                 else:
                     page.remove_template()
-                cursor.execute("DELETE FROM pages WHERE title = ?", (title,))
+                cursor.execute("DELETE FROM pages WHERE id = ?", (id,))
                 conn.commit()
             except Exception as e:
                 print(f"An error occurred while processing {title}: {e}")
-                cursor.execute("UPDATE pages SET status = 0, date = date + ? WHERE title = ?",
-                               (datetime.timedelta(hours=1), title))
+                cursor.execute("UPDATE pages SET status = 0, date = date + ? WHERE id = ?",
+                               (datetime.timedelta(hours=1), id))
                 conn.commit()
     else:
         time.sleep(60)
 except sqlite3.Error as e:
     print(f"An error occurred while interacting with the database: {e}")
+
+conn.close()
