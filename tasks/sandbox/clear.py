da99f295224797fa7190912f31ba357c0a843529
lokas <lokashafeek7755@gmail.com>
2023-01-27 10:00:16 +0200

    +sandbox

--- tasks/sandbox/clear.py
+++ tasks/sandbox/clear.py
@@ -1,8 +1,15 @@
 import pywikibot
 
-site = pywikibot.Site()
-page = pywikibot.Page(site, "ويكيبيديا:ملعب")
-text = "{{عنوان الملعب}}\n<!-- مرحبا! خذ راحتك في تجربة مهارتك في التنسيق والتحرير أسفل هذا السطر. هذه الصفحة لتجارب التعديل ، سيتم تفريغ هذه الصفحة كل 12 ساعة. -->"
-summary = "بوت:افراغ الصفحة تلقائيا!"
-page.text = text
-page.save(summary=summary)
+
+def main(*args: str) -> int:
+    site = pywikibot.Site()
+    page = pywikibot.Page(site, "ويكيبيديا:ملعب")
+    text = "{{عنوان الملعب}}\n<!-- مرحبا! خذ راحتك في تجربة مهارتك في التنسيق والتحرير أسفل هذا السطر. هذه الصفحة لتجارب التعديل ، سيتم تفريغ هذه الصفحة كل 12 ساعة. -->"
+    summary = "بوت:افراغ الصفحة تلقائيا!"
+    page.text = text
+    page.save(summary=summary)
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
