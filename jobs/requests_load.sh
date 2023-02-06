018cbb7dd65c01eb4ccbe4a86000a04dc3a10e3c
lokas <lokashafeek7755@gmail.com>
2023-02-06 21:01:29 +0200

    add jobs

---
+++ jobs/requests_load.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#set -euo pipefail
+
+. "$HOME"/repos/.venvs/lokas-bot-scripts/bin/activate
+
+export PYWIKIBOT_DIR="$HOME/repos"
+
+python3  "$HOME"/repos/tasks/requests/link_replacement/load.py
+
+# Exit the script after running all the Python files
+exit 0
