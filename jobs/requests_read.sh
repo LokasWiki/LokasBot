002f9246c11994011f8e5239ec55faf2a43a9d5e
lokas <lokashafeek7755@gmail.com>
2023-02-12 21:18:07 +0200

    add job

--- jobs/requests_read.sh
+++ jobs/requests_read.sh
@@ -7,6 +7,7 @@ export PYWIKIBOT_DIR="$HOME/repos"
 
 python3  "$HOME"/repos/tasks/requests/link_replacement/read.py
 python3  "$HOME"/repos/tasks/requests/template_distribution/read.py
+python3  "$HOME"/repos/tasks/requests/add_category/read.py
 
 # Exit the script after running all the Python files
 exit 0
