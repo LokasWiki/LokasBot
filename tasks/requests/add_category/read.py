2f4716e6e355cb550c40bebd602ce719334a755a
lokas <lokashafeek7755@gmail.com>
2023-02-12 19:52:33 +0200

    add read page

--- tasks/requests/add_category/read.py
+++ tasks/requests/add_category/read.py
@@ -9,19 +9,19 @@ from tasks.requests.core.database.models import Request, Status
 # Create an instance of the RequestsPage class
 site = pywikibot.Site()
 
-type_of_request = 2
+type_of_request = 1
 
 try:
 
     requests_page = RequestsPage(site)
-    requests_page.title = "ويكيبيديا:طلبات استبدال الوصلات"
+    requests_page.title = "ويكيبيديا:طلبات إضافة تصنيف"
     requests_page.header_text = "{{/ترويسة}}"
 
     requests_page.load_page()
 
     if requests_page.check_user_edits(1):
         scanner = RequestsScanner()
-        scanner.pattern = r"\*\s*\[\[:(?P<source>.*)\]\](?P<extra>.*)\[\[:(?P<destination>.*)\]\]"
+        scanner.pattern = r"\*\s*\[\[:تصنيف:(?P<source>.*)\]\](?P<extra>.*)\[\[:(?P<namespace_destination>.*):(?P<destination>.*)\]\]"
         scanner.scan(requests_page.get_page_text())
 
         if scanner.have_requests:
@@ -29,15 +29,14 @@ try:
             try:
                 with Session(engine) as session:
                     for request in scanner.requests:
-                        # source_page = pywikibot.Page(site, f"{request['source']}",ns=0)
-                        # destination_page = pywikibot.Page(site, f"{request['destination']}",ns=0)
-                        # if source_page.exists() and destination_page.exists() and source_page.namespace() == 0 and destination_page.namespace() == 0:
-
+                        to_namespace = 14
+                        if request['namespace_destination'] == "قالب":
+                            to_namespace = 10
                         request_model = Request(
                             from_title=request['source'],
                             to_title=request['destination'],
-                            from_namespace=0,
-                            to_namespace=0,
+                            from_namespace=14,
+                            to_namespace= to_namespace,
                             request_type=type_of_request
                         )
                         session.add(request_model)
