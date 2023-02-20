53f9d011cf2e0dd1ffa9932b9fb856770e8a55cb
lokas <lokashafeek7755@gmail.com>
2023-02-20 19:57:34 +0200

    fix some error

--- tasks/requests/cite/load.py
+++ tasks/requests/cite/load.py
@@ -1,3 +1,4 @@
+import traceback
 
 import pywikibot
 from sqlalchemy.orm import Session
@@ -27,14 +28,21 @@ try:
                 to_page = pywikibot.Page(site, request.from_name)
                 if to_page.exists():
                     page = to_page.title(with_ns=False)
+                    try:
+                        thread_number = 1
+                        conn, cursor = create_database_table()
+                        save_pages_to_db([page], conn, cursor, thread_number=thread_number)
+                        conn.close()
+                    except Exception as e:
+                        print(f"An error occurred: {e}")
             else:
-                to_page = pywikibot.Page(site, request.to_name)
-                if to_page.exists():
+                from_page = pywikibot.Page(site, request.from_name)
+                if from_page.exists():
                     database.query = """select page.page_title as prt_title from categorylinks
 inner join page on page.page_id = categorylinks.cl_from
 where cl_to in (select page.page_title from page where page_id = {})
 and cl_type = "page"
-and page.page_namespace = 0""".format(to_page.pageid)
+and page.page_namespace = 0""".format(from_page.pageid)
                     database.get_content_from_database()
                     gen = database.result
 
@@ -56,5 +64,7 @@ and page.page_namespace = 0""".format(to_page.pageid)
         except Exception as e:
             session.rollback()
             print("An error occurred while committing the changes:", e)
+            just_the_string = traceback.format_exc()
+            print(just_the_string)
 except Exception as e:
     print(f"An error occurred: {e}")
