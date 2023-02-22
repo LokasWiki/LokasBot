d49fb886479e784cfab81376044cf0f99b03f494
lokas <lokashafeek7755@gmail.com>
2023-02-22 12:36:40 +0200

    run in fri only

--- tasks/statistics/cite_q.py
+++ tasks/statistics/cite_q.py
@@ -1,3 +1,5 @@
+import time
+
 import pywikibot.page
 
 from module import UpdatePage, ArticleTables, index
@@ -19,49 +21,57 @@ GROUP BY q_iwl_title;"""
 file_path = 'stub/cite_q.txt'
 page_name = "ويكيبيديا:مصادر موثوقة/معاجم وقواميس وأطالس/إحصائيات"
 
-# Create an instance of the ArticleTables class
-tables = ArticleTables()
+# Get the current time and day of the week
+current_time = time.localtime()
+day_of_week = current_time.tm_wday
 
 
-def item(row, result, index):
-    name = str(row['q_iwl_title'], 'utf-8')
-    return f"[[d:{name}|{name}]]"
+# Check if it's fri
+if day_of_week == 4:
+
+    # Create an instance of the ArticleTables class
+    tables = ArticleTables()
 
 
-def item_title(row, result, index):
-    name = str(row['q_iwl_title'], 'utf-8')
-    name_of_itme = ""
-    try:
-        site = pywikibot.Site("wikidata")
-        page_item = pywikibot.page.ItemPage(site, name)
-        page_item.get()
-        if 'ar' in page_item.labels:
-            name_of_itme = page_item.labels['ar']
-
-    except:
-        print("error to get item from wikdata")
-
-    return name_of_itme
+    def item(row, result, index):
+        name = str(row['q_iwl_title'], 'utf-8')
+        return f"[[d:{name}|{name}]]"
 
 
-def end_row_in_main(result):
-    total = {'count_of_cites': 0}
+    def item_title(row, result, index):
+        name = str(row['q_iwl_title'], 'utf-8')
+        name_of_itme = ""
+        try:
+            site = pywikibot.Site("wikidata")
+            page_item = pywikibot.page.ItemPage(site, name)
+            page_item.get()
+            if 'ar' in page_item.labels:
+                name_of_itme = page_item.labels['ar']
 
-    for row in result:
-        for key in total:
-            total[key] += row[key]
-    return f"""\n|- class="sortbottom"\n! colspan="3" | عدد الاستشهادات الكلي\n! style="text-align:center;" | {total['count_of_cites']}\n"""
+        except:
+            print("error to get item from wikdata")
+
+        return name_of_itme
 
 
-columns = [
-    ("الرقم", None, index),
-    ("العنصر", None, item),
-    ("اسم الكتاب", None, item_title),
-    ("عدد مرات الاستشهاد", "count_of_cites"),
-]
+    def end_row_in_main(result):
+        total = {'count_of_cites': 0}
 
-tables.add_table("main_table", columns,end_row_text=end_row_in_main)
+        for row in result:
+            for key in total:
+                total[key] += row[key]
+        return f"""\n|- class="sortbottom"\n! colspan="3" | عدد الاستشهادات الكلي\n! style="text-align:center;" | {total['count_of_cites']}\n"""
 
-# Create an instance of the updater and update the page
-updater = UpdatePage(query, file_path, page_name, tables)
-updater.update()
+
+    columns = [
+        ("الرقم", None, index),
+        ("العنصر", None, item),
+        ("اسم الكتاب", None, item_title),
+        ("عدد مرات الاستشهاد", "count_of_cites"),
+    ]
+
+    tables.add_table("main_table", columns,end_row_text=end_row_in_main)
+
+    # Create an instance of the updater and update the page
+    updater = UpdatePage(query, file_path, page_name, tables)
+    updater.update()
