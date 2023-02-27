882b7846facc6a4ff9739c594451409feb7e6737
lokas <lokashafeek7755@gmail.com>
2023-02-27 10:37:29 +0200

    split code

--- tasks/check_usernames/check/modules.py
+++ tasks/check_usernames/check/modules.py
@@ -3,8 +3,60 @@ import wikitextparser as wtp
 import pywikibot.flow
 
 
+class SendAlert:
+    def __init__(self, user_name, has_reason, reason, site):
+        self.user_name = user_name
+        self.has_reason = has_reason
+        self.reason = reason
+        self.site = site
+
+    def start_send(self):
+        # Retrieve the user talk page
+        user = pywikibot.User(self.site, self.user_name)
+
+        # Get the user page for the user
+        talk_page = user.getUserTalkPage()
+
+        if talk_page.is_flow_page():
+            board = pywikibot.flow.Board(talk_page)
+
+            # Add a new section to the page
+            title = 'اسم المستخدم مخالف'
+            if self.has_reason:
+                content = "{{نسخ:تنبيه اسم مستخدم|REASON}}".replace("REASON", str(self.reason).strip())
+            else:
+                content = "{{نسخ:تنبيه اسم مستخدم}}"
+
+            try:
+                topic = board.new_topic(title, content)
+
+            except Exception as error:
+                print(f'Error saving page: {error}')
+
+        else:
+            pass
+            # Add a new section to the page
+            text = talk_page.text
+            text += '\n'
+            if self.has_reason:
+                text += "{{نسخ:تنبيه اسم مستخدم|REASON}}----[[مستخدم:Dr-Taher|Dr-Taher]] ([[نقاش المستخدم:Dr-Taher|نقاش]]) {{safesubst:#وقت:G:i، j F Y}}  (ت ع م)".replace(
+                    "REASON", str(self.reason).strip())
+            else:
+                text += "{{نسخ:تنبيه اسم مستخدم}}----[[مستخدم:Dr-Taher|Dr-Taher]] ([[نقاش المستخدم:Dr-Taher|نقاش]]) {{safesubst:#وقت:G:i، j F Y}}  (ت ع م)"
+
+            try:
+                # Save the edited page
+                talk_page.text = text
+                # Save the page
+                talk_page.save(summary="بوت:تنبيه اسم مخالف", minor=False)
+            except Exception as error:
+                print(f'Error saving page: {error}')
+
+
 class ReadUsers:
     def __init__(self, site, page_title):
+        self.page = None
+        self.parsed = None
         self.site = site
         self.page_title = page_title
         self.text = None
@@ -33,52 +85,8 @@ class ReadUsers:
 
     def start_send_alert(self):
         for user in self.users:
-            if str(user['username']).strip().lower() == str("Lokas7755").strip().lower():
-
-                self.send_alert(user['username'], user['has_reason'], user['reason'])
-
-    def send_alert(self, user_name, has_reason, reason):
-
-        # Retrieve the user talk page
-        user = pywikibot.User(self.site, user_name)
-
-        # Get the user page for the user
-        talk_page = user.getUserTalkPage()
-
-        if talk_page.is_flow_page():
-            board = pywikibot.flow.Board(talk_page)
-
-            # Add a new section to the page
-            title = 'اسم المستخدم مخالف'
-            if has_reason:
-                content = "{{نسخ:تنبيه اسم مستخدم|REASON}}".replace("REASON",str(reason).strip())
-            else:
-                content = "{{نسخ:تنبيه اسم مستخدم}}"
-
             try:
-                topic = board.new_topic(title, content)
-
-            except Exception as error:
-                print(f'Error saving page: {error}')
-
-        else:
-            pass
-            # Add a new section to the page
-            text = talk_page.text
-            text += '\n'
-            if has_reason:
-                text += "{{نسخ:تنبيه اسم مستخدم|REASON}}----[[مستخدم:Dr-Taher|Dr-Taher]] ([[نقاش المستخدم:Dr-Taher|نقاش]]) {{safesubst:#وقت:G:i، j F Y}}  (ت ع م)".replace("REASON",str(reason).strip())
-            else:
-                text += "{{نسخ:تنبيه اسم مستخدم}}----[[مستخدم:Dr-Taher|Dr-Taher]] ([[نقاش المستخدم:Dr-Taher|نقاش]]) {{safesubst:#وقت:G:i، j F Y}}  (ت ع م)"
-
-            try:
-                # Save the edited page
-
-                talk_page.text = text
-
-                # Save the page
-                talk_page.save(summary="بوت:تنبيه اسم مخالف",minor=False)
-            except Exception as error:
-                print(f'Error saving page: {error}')
-
-
+                send_obj = SendAlert(user['username'], user['has_reason'], user['reason'], site=self.site)
+                send_obj.start_send()
+            except:
+                print("can`t send  alert")
