5d2e18b6f9759cccdea5a36d0c82b598b9146739
lokas <lokashafeek7755@gmail.com>
2023-03-08 17:12:44 +0200

    add auto_update/cite_q.py

---
+++ tasks/statistics/auto_update/cite_q.py
@@ -0,0 +1,41 @@
+import pywikibot
+import wikitextparser as wtp
+
+from tasks.statistics.cite_q import page_name, columns, end_row_in_main, query, file_path
+from core.utils.helpers import prepare_str
+from tasks.statistics.module import ArticleTables, UpdatePage
+
+
+def check(page_title):
+    template_name = prepare_str("حدث الصفحة بالبوت")
+    site = pywikibot.Site()
+    page = pywikibot.Page(site, page_title)
+    status = False
+    if page.exists():
+        parsed = wtp.parse(page.text)
+        for template in parsed.templates:
+            if prepare_str(template.name) == template_name:
+                for arg in template.arguments:
+                    if prepare_str(arg.name) == prepare_str("حدث الصفحة"):
+                        if prepare_str(arg.value) == prepare_str("نعم"):
+                            status = True
+                            break
+
+    return status
+
+
+def main(*args: str) -> int:
+    if check(page_name):
+        # todo:make this as def and call it from main
+        # Create an instance of the ArticleTables class
+        tables = ArticleTables()
+        tables.add_table("main_table", columns, end_row_text=end_row_in_main)
+        # Create an instance of the updater and update the page
+        updater = UpdatePage(query, file_path, page_name, tables)
+        updater.update()
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
