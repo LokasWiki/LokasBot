4a51f425293e485440b0613bcca90013c9c1784a
lokas <lokashafeek7755@gmail.com>
2023-03-09 22:35:54 +0200

    ارجاع الكرون جوب القديمة لحين رفع التحديث الجديد

--- jobs/webcite_read.sh
+++ jobs/webcite_read.sh
@@ -7,10 +7,6 @@ export PYWIKIBOT_DIR="$HOME/repos"
 
 export PYTHONPATH="${PYTHONPATH}:$HOME/repos"
 
-# maintenance tasks
-python3  "$HOME"/repos/tasks/maintenance/read.py "$1"
-python3  "$HOME"/repos/tasks/maintenance/task/add/orphan_remove.py "$1"
-# web cite tasks
 python3  "$HOME"/repos/tasks/webcite/read.py "$1"
 
 # Exit the script after running all the Python files
