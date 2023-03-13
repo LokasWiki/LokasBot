06b23ff1a5307f62e01403c6d3eb1b3bc4369be8
lokas <lokashafeek7755@gmail.com>
2023-03-13 11:46:13 +0200

    add try except to  Pipeline

--- core/utils/pipeline.py
+++ core/utils/pipeline.py
@@ -1,3 +1,6 @@
+import logging
+
+
 class Pipeline:
     def __init__(self, page, text, summary, steps, extra_steps):
         self.page = page
@@ -9,13 +12,22 @@ class Pipeline:
 
     def process(self):
         for step in self.steps:
-            obj = step(self.page, self.text, self.summary)
-            self.text, self.summary = obj()
+
+            try:
+                # to skip if some one bot not all
+                obj = step(self.page, self.text, self.summary)
+                self.text, self.summary = obj()
+            except Exception as e:
+                logging.exception(e)
 
         if self.hasChange():
             for step in self.extra_steps:
-                obj = step(self.page, self.text, self.summary)
-                self.text, self.summary = obj()
+                try:
+                    # to skip if some one bot not all
+                    obj = step(self.page, self.text, self.summary)
+                    self.text, self.summary = obj()
+                except Exception as e:
+                    logging.exception(e)
 
         return self.text, self.summary
 
