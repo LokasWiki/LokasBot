74358f7fd934ca9d75524d45637e83772b7fae66
lokas <lokashafeek7755@gmail.com>
2023-03-13 11:27:21 +0200

    update portals_merge.py task

--- tasks/maintenance/task/portals_merge.py
+++ tasks/maintenance/task/portals_merge.py
@@ -3,6 +3,11 @@ import traceback
 import pywikibot
 from pywikibot import pagegenerators
 
+from tasks.maintenance.bots.dead_end import DeadEnd
+from tasks.maintenance.bots.rename_template_parameters import RenameTemplateParameters
+from tasks.maintenance.bots.template_redirects import TemplateRedirects
+from tasks.maintenance.bots.underlinked import UnderLinked
+from tasks.maintenance.bots.unreferenced import Unreferenced
 from tasks.maintenance.module import check_status, Pipeline
 
 # bots
@@ -29,29 +34,37 @@ for cat in categories_names:
         steps = [
             UnreviewedArticle,
             HasCategories,
+            PortalsBar,
+            Unreferenced,
+            Orphan,
+            DeadEnd,
+            UnderLinked,
             PortalsMerge,
             PortalsBar,
-            # Unreferenced,
-            Orphan,
-            # DeadEnd,
-            # Underlinked
         ]
         extra_steps = [
-
+            TemplateRedirects,
+            RenameTemplateParameters
         ]
         try:
             if page.exists() and (not page.isRedirectPage()):
                 text = page.text
-                summary = "بوت:صيانة V4.8.6"
+                summary = "بوت:صيانة V5.5.0"
                 pipeline = Pipeline(page, text, summary, steps, extra_steps)
                 processed_text, processed_summary = pipeline.process()
                 # write processed text back to the page
-                if pipeline.hasChange() and check_status():
+                if pipeline.hasChange() and check_status("مستخدم:LokasBot/إيقاف مهمة صيانة المقالات"):
                     print("start save " + page.title())
                     page.text = processed_text
+                    # to remove duplicate summary
+                    if str(processed_summary).count("، تعريب"):
+                        processed_summary = str(processed_summary).replace("، تعريب", "")
+                        processed_summary += "، تعريب"
+
                     page.save(summary=processed_summary)
                 else:
                     print("page not changed " + page.title())
+
         except Exception as e:
             print(f"An error occurred while processing {page.title()}: {e}")
             just_the_string = traceback.format_exc()
