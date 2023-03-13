e85bb4153676793e4687d60f6a8592977d03a457
lokas <lokashafeek7755@gmail.com>
2023-03-13 12:03:53 +0200

    fix underlinked.py

--- tasks/maintenance/bots/underlinked.py
+++ tasks/maintenance/bots/underlinked.py
@@ -1,3 +1,5 @@
+import logging
+
 import pywikibot
 from core.utils.disambiguation import Disambiguation
 from core.utils.helpers import prepare_str
@@ -16,6 +18,7 @@ class UnderLinked:
             "Underlinked",
             "ويكي"
         ]
+        self.parsed = wtp.parse(self.text)
 
     def __call__(self):
         disambiguation = Disambiguation(self.page.title(), self.text)
@@ -35,10 +38,9 @@ class UnderLinked:
         """
         This method adds the {{وصلات قليلة}} template to the page if it doesn't already exist.
         """
-        parsed = wtp.parse(self.text)
         found = False
         for needed_template in self.templates:
-            for template in parsed.templates:
+            for template in self.parsed.templates:
                 if prepare_str(template.name) == prepare_str(needed_template):
                     found = True
                     break
@@ -55,10 +57,9 @@ class UnderLinked:
         """
            This method removes the {{وصلات قليلة}} template from the page if it exists.
            """
-        parsed = wtp.parse(self.text)
         new_text = self.text
         for needed_template in self.templates:
-            for template in parsed.templates:
+            for template in self.parsed.templates:
                 if prepare_str(template.name) == prepare_str(needed_template):
                     new_text = str(new_text).replace(str(template), "")
 
@@ -69,21 +70,26 @@ class UnderLinked:
     def check(self):
         # todo:split this code
         links_list = []
-        parsed = wtp.parse(self.text)
-        links = parsed.wikilinks
+        links = self.parsed.wikilinks
         for link in links:
-            temp_page = pywikibot.Page(self.page.site, link.title)
-            if temp_page.exists() and temp_page.namespace() == 0:
-                if temp_page.isRedirectPage():
-                    temp_page_redirect = temp_page.getRedirectTarget()
-                    if temp_page_redirect.exists() and temp_page_redirect.namespace() == 0:
-                        links_list.append(prepare_str(link.title))
-                        if len(list(set(links_list))) >= 4:
-                            break
-                else:
-                    links_list.append(prepare_str(link.title))
-                    if len(list(set(links_list))) >= 4:
-                        break
+            try:
+                temp_page = pywikibot.Page(self.page.site, link.title)
+                if temp_page.exists():
+                    if temp_page.namespace() == 0:
+                        if temp_page.isRedirectPage():
+                            temp_page_redirect = temp_page.getRedirectTarget()
+                            if temp_page_redirect.exists() and temp_page_redirect.namespace() == 0:
+                                links_list.append(prepare_str(link.title))
+                                if len(list(set(links_list))) >= 4:
+                                    break
+                        else:
+                            links_list.append(prepare_str(link.title))
+                            if len(list(set(links_list))) >= 4:
+                                break
+
+            except Exception as e:
+                logging.exception(e)
+
         status = False
         if 1 <= len(list(set(links_list))) < 3:
             status = True
--- tasks/maintenance/test.py
+++ tasks/maintenance/test.py
@@ -1,19 +1,25 @@
 import pywikibot
 
-from tasks.maintenance.bots.portals_merge import PortalsMerge
+from tasks.maintenance.bots.rename_template_parameters import RenameTemplateParameters
+from tasks.maintenance.bots.template_redirects import TemplateRedirects
+
 site = pywikibot.Site()
 
 # page_title = "الشاوية (المغرب)"
-page_title = "مستخدم:لوقا/ملعب 24"
+page_title = "مستخدم:لوقا/ملعب 25"
 
 page = pywikibot.Page(site, page_title)
 # rev_id = 60534506 # some revision id
 
 main_summary = "بوت:صيانة V4.8.10"
 
-obj = PortalsMerge(page, page.text, main_summary)
+obj1 = TemplateRedirects(page, page.text, main_summary)
+text1, summary1 = obj1()
+obj = RenameTemplateParameters(page, text1, main_summary)
 # obj = PortalsMerge(page, page.getOldVersion(rev_id), main_summary)
 text, summary = obj()
 
-page.text = text
-page.save(summary)
+print(text)
+#
+# page.text = text
+# page.save(summary)
