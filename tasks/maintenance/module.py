672d899000460dfaa3d47057b2b064d815c17373
lokas <lokashafeek7755@gmail.com>
2023-03-13 10:52:08 +0200

    add task to bot

--- tasks/maintenance/module.py
+++ tasks/maintenance/module.py
@@ -16,6 +16,7 @@ from tasks.maintenance.bots.has_categories import HasCategories
 from tasks.maintenance.bots.orphan import Orphan
 from tasks.maintenance.bots.portals_bar import PortalsBar
 from tasks.maintenance.bots.portals_merge import PortalsMerge
+from tasks.maintenance.bots.rename_template_parameters import RenameTemplateParameters
 from tasks.maintenance.bots.template_redirects import TemplateRedirects
 from tasks.maintenance.bots.underlinked import UnderLinked
 from tasks.maintenance.bots.unreferenced import Unreferenced
@@ -126,17 +127,23 @@ def process_article(site, cursor, conn, id, title, thread_number):
                         extra_steps = [
                             PortalsMerge,
                             PortalsBar,
-                            TemplateRedirects
+                            TemplateRedirects,
+                            RenameTemplateParameters
                         ]
                         if page.exists() and (not page.isRedirectPage()):
                             text = page.text
-                            summary = "بوت:صيانة V5.4.0"
+                            summary = "بوت:صيانة V5.5.0"
                             pipeline = Pipeline(page, text, summary, steps, extra_steps)
                             processed_text, processed_summary = pipeline.process()
                             # write processed text back to the page
                             if pipeline.hasChange() and check_status("مستخدم:LokasBot/إيقاف مهمة صيانة المقالات"):
                                 print("start save " + page.title())
                                 page.text = processed_text
+                                # to remove duplicate summary
+                                if str(processed_summary).count("، تعريب"):
+                                    processed_summary = str(processed_summary).replace("، تعريب", "")
+                                    processed_summary += "، تعريب"
+
                                 page.save(summary=processed_summary)
                             else:
                                 print("page not changed " + page.title())
