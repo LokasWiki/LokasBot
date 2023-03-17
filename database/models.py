939270dadea21383c2954748264290e756994bee
lokas <lokashafeek7755@gmail.com>
2023-03-17 14:25:36 +0200

    update models.py

--- database/models.py
+++ database/models.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 import enum
 
-from .engine import maintenance_engine, webcite_engine, statistics_engine
+from .engine import engine
 
 
 class Base(DeclarativeBase):
@@ -34,21 +34,19 @@ class Page(Base):
     # todo: comment columns will add in next version
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(String(255))
-    # thread_number: Mapped[int] = mapped_column(INTEGER)
-    thread: Mapped[int] = mapped_column(INTEGER)
-    # status: Mapped[Status] = mapped_column(insert_default=Status.PENDING)
-    status: Mapped[int] = mapped_column(insert_default=0)
+    thread_number: Mapped[int] = mapped_column(INTEGER)
+    status: Mapped[Status] = mapped_column(insert_default=Status.PENDING)
     date: Mapped[datetime] = mapped_column(insert_default=func.now())
 
-    # create_date: Mapped[datetime] = mapped_column(insert_default=func.now())
-    # update_date: Mapped[datetime] = mapped_column(server_default=func.now(), onupdate=func.current_timestamp())
-    # task_name: Mapped[TaskName] = mapped_column(insert_default=TaskName.MAINTENANCE)
+    create_date: Mapped[datetime] = mapped_column(insert_default=func.now())
+    update_date: Mapped[datetime] = mapped_column(server_default=func.now(), onupdate=func.current_timestamp())
+    task_name: Mapped[TaskName] = mapped_column(insert_default=TaskName.MAINTENANCE)
 
     def __repr__(self) -> str:
         return f"pages(id={self.id!r}, title={self.title!r})"
 
 
-class Statistic(BaseS):
+class Statistic(Base):
     __tablename__ = "statistics"
     id: Mapped[int] = mapped_column(primary_key=True)
     key: Mapped[str] = mapped_column(String(255), unique=True)
@@ -58,6 +56,4 @@ class Statistic(BaseS):
         return f"statistics(id={self.id!r}, key={self.key!r}), value={self.value!r})"
 
 
-Base.metadata.create_all(webcite_engine)
-Base.metadata.create_all(maintenance_engine)
-BaseS.metadata.create_all(statistics_engine)
+Base.metadata.create_all(engine)
