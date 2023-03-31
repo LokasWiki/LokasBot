4800117386b48d1c702856f29c1e766e124cccea
lokas <lokashafeek7755@gmail.com>
2023-03-31 21:34:04 +0200

    add protection bot

--- tasks/maintenance/bots/protection.py
+++ tasks/maintenance/bots/protection.py
@@ -1,8 +1,6 @@
-import re
-
-import pywikibot
-from core.utils.disambiguation import Disambiguation
 import wikitextparser as wtp
+
+from core.utils.helpers import prepare_str
 
 
 class Protection:
@@ -34,6 +32,7 @@ class Protection:
             "حماية تخريب"
         ]
         self.type_of_protection = None
+        self.parsed = wtp.parse(self.text)
         self.summary = summary
 
     def __call__(self):
@@ -47,27 +46,40 @@ class Protection:
 
     def add_template(self):
         """
-        This method adds the {{بذرة غير مصنفة}} template to the page if it doesn't already exist.
+        This method adds the {{محمية}} template to the page if it doesn't already exist.
         """
-        pass
-        # template = re.compile(r"{{بذرة غير مصنفة(?:\|[^}]+)?}}")
-        # if not template.search(self.text):
-        #     text = self.text
-        #     text += "\n"
-        #     text += "{{بذرة غير مصنفة|تاريخ ={{نسخ:شهر وسنة}}}}"
-        #
-        #     self.text = text
-        #     self.summary += "، أضاف  وسم [[:تصنيف:مقالات غير مصنفة|غير مصنفة]]"
+        # todo:add away to check old template attr
+        found = False
+        for needed_template in self.templates:
+            for template in self.parsed.templates:
+                if prepare_str(template.name) == prepare_str(needed_template):
+                    found = True
+                    break
+
+        if not found:
+            if self.page.isRedirectPage():
+                new_text = self.text
+                new_text += "\n"
+                new_text += "{{محمية|تحويلة}}"
+            else:
+                if self.type_of_protection is not None:
+                    new_text = "{{محمية|" + str(self.type_of_protection) + "}}"
+                else:
+                    new_text = "{{محمية}}"
+                new_text += "\n"
+                new_text += self.text
+
+            self.text = new_text
+            self.summary += "، بوت:إضافة قالب حماية"
 
     def remove_template(self):
         """
            This method removes the {{محمية}} template from the page if it exists.
            """
-        parsed = wtp.parse(self.text)
         new_text = self.text
         for needed_template in self.templates:
-            for template in parsed.templates:
-                if template.name.strip().lower() == needed_template.strip().lower():
+            for template in self.parsed.templates:
+                if prepare_str(template.name) == prepare_str(needed_template):
                     new_text = str(new_text).replace(str(template), "")
 
         if new_text != self.text:
@@ -81,11 +93,16 @@ class Protection:
         # The dictionary is not empty that mean page have protection
         if bool(protection):
             has_protection = True
-            # check date
-            # check page type
-            # check move if found only
-            # check edit if found only
-            # check edit type
-            print("The dictionary is not empty")
+            if 'edit' in protection and protection['edit'] is not None:
+                if 'autoconfirmed' in protection['edit']:
+                    self.type_of_protection = 1
+                elif 'editautoreviewprotected' in protection['edit']:
+                    self.type_of_protection = 4
+                elif 'editeditorprotected' in protection['edit']:
+                    self.type_of_protection = 2
+                elif 'sysop' in protection['edit']:
+                    self.type_of_protection = 3
+            elif 'move' in protection and protection['move'] is not None:
+                self.type_of_protection = "نقل"
 
         return has_protection
--- tests/tasks/maintenance/bots/protection_test.py
+++ tests/tasks/maintenance/bots/protection_test.py
@@ -1,6 +1,6 @@
 import unittest
-from unittest.mock import MagicMock, Mock
-from unittest.mock import patch
+from unittest.mock import MagicMock
+
 from tasks.maintenance.bots.protection import Protection
 
 
@@ -31,10 +31,11 @@ class MyTestCase(unittest.TestCase):
             "حماية تخريب"
         ]
 
-    def test_run_not_have_protection_and_not_have_template(self):
+    def test_check_page_and_this_page_not_have_protection(self):
         page = unittest.mock.Mock()
         page.title.return_value = "Example Page"
         page.protection.return_value = {}
+        page.isRedirectPage.return_value = False
         text = "Some text without the template."
         summary = "Test summary"
 
@@ -44,31 +45,145 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(new_text, "Some text without the template.")
         self.assertEqual(new_summary, "Test summary")
 
-    def test_run_not_have_protection_and_have_template(self):
+        # for Redirect
+        page.isRedirectPage.return_value = True
 
-        list_of_stub_template = []
+        pb = Protection(page, text, summary)
+        new_text, new_summary = pb.__call__()
 
-        for template in self.templates:
-            list_of_stub_template.append("{{" + template + "|1}}")
-            list_of_stub_template.append("{{" + template + "|2}}")
-            list_of_stub_template.append("{{" + template + "|3}}")
-            list_of_stub_template.append("{{" + template + "|4}}")
-            list_of_stub_template.append("{{" + template + "|تحويلة}}")
-            list_of_stub_template.append("{{" + template + "|نقل}}")
-            list_of_stub_template.append("{{" + template + "}}")
+        self.assertEqual(new_text, "Some text without the template.")
+        self.assertEqual(new_summary, "Test summary")
 
-        for template in list_of_stub_template:
-            page = unittest.mock.Mock()
-            page.title.return_value = "Example Page"
-            page.protection.return_value = {}
-            text = f"{template}Some text without the template."
-            summary = "Test summary"
+    def test_check_page_and_this_page_not_have_protection_but_have_protection_banner(self):
+        page = unittest.mock.Mock()
+        page.title.return_value = "Example Page"
+        page.protection.return_value = {}
+        page.isRedirectPage.return_value = False
+        text = "{{محمية}}Some text without the template."
+        summary = "Test summary"
 
-            pb = Protection(page, text, summary)
-            new_text, new_summary = pb.__call__()
+        pb = Protection(page, text, summary)
+        new_text, new_summary = pb.__call__()
 
-            self.assertEqual(new_text, text.replace(template, ""),f"error in template {template}")
-            self.assertEqual(new_summary, "Test summary، إزالة قالب الحماية",f"error in template {template}")
+        self.assertEqual(new_text, "Some text without the template.")
+        self.assertEqual(new_summary, "Test summary، إزالة قالب الحماية")
+
+        # for Redirect
+        page.isRedirectPage.return_value = True
+
+        pb = Protection(page, text, summary)
+        new_text, new_summary = pb.__call__()
+
+        self.assertEqual(new_text, "Some text without the template.")
+        self.assertEqual(new_summary, "Test summary، إزالة قالب الحماية")
+
+    def test_check_page_and_this_page_have_autoconfirmed_protection_but_not_have_protection_banner(self):
+        page = unittest.mock.Mock()
+        page.title.return_value = "Example Page"
+        page.protection.return_value = {'edit': ('autoconfirmed', 'infinity'), 'move': ('autoconfirmed', 'infinity')}
+        page.isRedirectPage.return_value = False
+        text = "Some text without the template."
+        summary = "Test summary"
+
+        pb = Protection(page, text, summary)
+        new_text, new_summary = pb.__call__()
+
+        self.assertEqual(new_text, "{{محمية|1}}\nSome text without the template.")
+        self.assertEqual(new_summary, "Test summary، بوت:إضافة قالب حماية")
+
+        # for move only
+        page.protection.return_value = {'move': ('autoconfirmed', 'infinity')}
+        page.isRedirectPage.return_value = False
+        text = "Some text without the template."
+        summary = "Test summary"
+
+        pb = Protection(page, text, summary)
+        new_text, new_summary = pb.__call__()
+
+        self.assertEqual(new_text, "{{محمية|نقل}}\nSome text without the template.")
+        self.assertEqual(new_summary, "Test summary، بوت:إضافة قالب حماية")
+
+        # for Redirect
+        page.isRedirectPage.return_value = True
+
+        pb = Protection(page, text, summary)
+        new_text, new_summary = pb.__call__()
+
+        self.assertEqual(new_text, "Some text without the template.\n{{محمية|تحويلة}}")
+        self.assertEqual(new_summary, "Test summary، بوت:إضافة قالب حماية")
+
+    def test_check_page_and_this_page_have_editautoreviewprotected_protection_but_not_have_protection_banner(self):
+        page = unittest.mock.Mock()
+        page.title.return_value = "Example Page"
+        page.protection.return_value = {'edit': ('editautoreviewprotected', 'infinity'),
+                                        'move': ('editautoreviewprotected', 'infinity')}
+        page.isRedirectPage.return_value = False
+        text = "Some text without the template."
+        summary = "Test summary"
+
+        pb = Protection(page, text, summary)
+        new_text, new_summary = pb.__call__()
+
+        self.assertEqual(new_text, "{{محمية|4}}\nSome text without the template.")
+        self.assertEqual(new_summary, "Test summary، بوت:إضافة قالب حماية")
+
+        # for move only
+        page.protection.return_value = {'move': ('editautoreviewprotected', 'infinity')}
+        page.isRedirectPage.return_value = False
+        text = "Some text without the template."
+        summary = "Test summary"
+
+        pb = Protection(page, text, summary)
+        new_text, new_summary = pb.__call__()
+
+        self.assertEqual(new_text, "{{محمية|نقل}}\nSome text without the template.")
+        self.assertEqual(new_summary, "Test summary، بوت:إضافة قالب حماية")
+
+        # for Redirect
+        page.isRedirectPage.return_value = True
+
+        pb = Protection(page, text, summary)
+        new_text, new_summary = pb.__call__()
+
+        self.assertEqual(new_text, "Some text without the template.\n{{محمية|تحويلة}}")
+        self.assertEqual(new_summary, "Test summary، بوت:إضافة قالب حماية")
+
+    def test_check_page_and_this_page_have_editeditorprotected_protection_but_not_have_protection_banner(self):
+        page = unittest.mock.Mock()
+        page.title.return_value = "Example Page"
+        page.protection.return_value = {'edit': ('editeditorprotected', 'infinity'),
+                                        'move': ('editeditorprotected', 'infinity')}
+        page.isRedirectPage.return_value = False
+        text = "Some text without the template."
+        summary = "Test summary"
+
+        pb = Protection(page, text, summary)
+        new_text, new_summary = pb.__call__()
+
+        self.assertEqual(new_text, "{{محمية|2}}\nSome text without the template.")
+        self.assertEqual(new_summary, "Test summary، بوت:إضافة قالب حماية")
+
+        # for move only
+        page.protection.return_value = {'move': ('editeditorprotected', 'infinity')}
+        page.isRedirectPage.return_value = False
+        text = "Some text without the template."
+        summary = "Test summary"
+
+        pb = Protection(page, text, summary)
+        new_text, new_summary = pb.__call__()
+
+        self.assertEqual(new_text, "{{محمية|نقل}}\nSome text without the template.")
+        self.assertEqual(new_summary, "Test summary، بوت:إضافة قالب حماية")
+
+        # for Redirect
+        page.isRedirectPage.return_value = True
+
+        pb = Protection(page, text, summary)
+        new_text, new_summary = pb.__call__()
+
+        self.assertEqual(new_text, "Some text without the template.\n{{محمية|تحويلة}}")
+        self.assertEqual(new_summary, "Test summary، بوت:إضافة قالب حماية")
+
 
 if __name__ == '__main__':
     unittest.main()
