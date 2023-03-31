fc09aa6d8b455b0452bd1586f0d0f162df2e88be
lokas <lokashafeek7755@gmail.com>
2023-03-31 20:09:54 +0200

    update setup-venvs.sh

--- bin/setup-venvs.sh
+++ bin/setup-venvs.sh
@@ -10,9 +10,6 @@ function setup-venv {
     python3 -m venv "$HOME/repos/.venvs/$1"
     . "$HOME/repos/.venvs/$1/bin/activate"
     python3 -m pip install -U pip setuptools wheel
-    python -m pip install "requests>=2.20.1"
-    python -m pip install "mwparserfromhell>=0.5.0"
-    python -m pip install "wikitextparser>=0.47.5"
     python -m pip install pywikibot
     # shellcheck disable=SC2086
     python3 -m pip install $2
