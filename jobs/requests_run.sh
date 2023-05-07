31dae70fffb14c04b010646c7fa24d369a2932f0
lokas <lokashafeek7755@gmail.com>
2023-05-07 11:11:51 +0200

    add to jobs

--- jobs/requests_run.sh
+++ jobs/requests_run.sh
@@ -12,6 +12,7 @@ python3  "$HOME"/repos/tasks/requests/template_distribution/run.py
 python3  "$HOME"/repos/tasks/requests/portal_distribution/run.py
 python3  "$HOME"/repos/tasks/requests/replace_template/run.py
 python3  "$HOME"/repos/tasks/requests/add_category/run.py
+python3  "$HOME"/repos/tasks/requests/remove/run.py
 
 # Exit the script after running all the Python files
 exit 0
