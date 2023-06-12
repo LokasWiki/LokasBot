589301e5a24d3714770036600f4553048aed9aeb
lokas <lokashafeek7755@gmail.com>
2023-06-12 14:23:42 +0200

    add get_user_name def from signature str

--- tasks/distribute_medals/run.py
+++ tasks/distribute_medals/run.py
@@ -1,8 +1,19 @@
 import pywikibot
+import wikitextparser as wtp
 
 from data import list_of_distribute_medals
 from module import SendTemplate, SignatureScanner, SignaturePage
 
+
+def get_user_name(signature):
+    try:
+        parsed = wtp.parse(signature)
+        link = parsed.wikilinks[0]
+        namespace, user_name = link.title.split(":")
+        return user_name
+    except Exception as e:
+        print(f"An error occurred while processing get_user_name def with {signature}: {e}")
+        return None
 
 def main(*args: str) -> int:
     site = pywikibot.Site()
@@ -14,11 +25,17 @@ def main(*args: str) -> int:
     scanner.pattern = r"\*(?P<signature>.*?)(?=\*|$)"
     scanner.scan(page.get_page_text())
     if scanner.have_requests:
+        signatures = []
+        for request in scanner.requests:
+            signatures.append({
+                'signature': request['signature'],
+                'user_name': get_user_name(request['signature'])
+            })
         for page_data in list_of_distribute_medals:
             try:
                 print("start get " + str(page_data['number']))
                 # # Create a SendTemplate object with the page data
-                obj = SendTemplate(input_dict=page_data, signature_list=scanner.requests)
+                obj = SendTemplate(input_dict=page_data, signature_list=signatures)
                 # Send the template to the user
                 obj.send()
             except Exception as e:
