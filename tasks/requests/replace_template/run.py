f8df30143cfaa29a9f90c31f5b732842ee179a77
lokas <lokashafeek7755@gmail.com>
2023-06-26 12:18:57 +0200

    add check_status

--- tasks/requests/replace_template/run.py
+++ tasks/requests/replace_template/run.py
@@ -6,7 +6,7 @@ import wikitextparser as wtp
 from sqlalchemy import select, func, distinct
 from sqlalchemy.orm import Session
 
-from core.utils.helpers import prepare_str
+from core.utils.helpers import prepare_str, check_status
 from tasks.requests.core.database.engine import engine
 from tasks.requests.core.database.models import Request, Status, Page
 
@@ -22,36 +22,38 @@ try:
                                              Request.request_type == type_of_request).group_by(Request).having(
         func.count(Page.id) == func.count(distinct(Page.id))).limit(20)
 
-    for request in session.scalars(stmt):
+    if check_status("ويكيبيديا:طلبات استبدال القوالب/إيقاف"):
 
-        template_from = request.from_title
-        template_to = request.to_title
+        for request in session.scalars(stmt):
 
-        pages = session.query(Page).filter(Page.request == request, Page.status == Status.PENDING).limit(1000).all()
+            template_from = request.from_title
+            template_to = request.to_title
 
-        for page in pages:
-            try:
-                p = pywikibot.Page(site, title=str(page.page_name), ns=page.namespace)
-                if p.exists():
-                    temp_text = p.text
-                    parsed = wtp.parse(p.text)
-                    for template in parsed.templates:
-                        if prepare_str(template.name) == prepare_str(template_from):
-                            temp_template = copy.deepcopy(template)
-                            temp_template.name = str(template_to).replace("_", ' ')
-                            temp_text = temp_text.replace(str(template), str(temp_template))
+            pages = session.query(Page).filter(Page.request == request, Page.status == Status.PENDING).limit(1000).all()
 
-                    p.text = temp_text
-                    p.save(
-                        summary="بوت:[[ويكيبيديا:طلبات استبدال القوالب]] استبدال [[قالب:" + template_from + "]] ب [[قالب:" + template_to + "]]"
-                    )
-                    page.status = Status.COMPLETED
-                    session.commit()
-            except Exception as e:
-                print(f"An error occurred where save : {e}")
-                just_the_string = traceback.format_exc()
-                print(just_the_string)
-                session.rollback()
+            for page in pages:
+                try:
+                    p = pywikibot.Page(site, title=str(page.page_name), ns=page.namespace)
+                    if p.exists() and check_status("ويكيبيديا:طلبات استبدال القوالب/إيقاف"):
+                        temp_text = p.text
+                        parsed = wtp.parse(p.text)
+                        for template in parsed.templates:
+                            if prepare_str(template.name) == prepare_str(template_from):
+                                temp_template = copy.deepcopy(template)
+                                temp_template.name = str(template_to).replace("_", ' ')
+                                temp_text = temp_text.replace(str(template), str(temp_template))
+
+                        p.text = temp_text
+                        p.save(
+                            summary="بوت:[[ويكيبيديا:طلبات استبدال القوالب]] استبدال [[قالب:" + template_from + "]] ب [[قالب:" + template_to + "]]  (v2.0.0)"
+                        )
+                        page.status = Status.COMPLETED
+                        session.commit()
+                except Exception as e:
+                    print(f"An error occurred where save : {e}")
+                    just_the_string = traceback.format_exc()
+                    print(just_the_string)
+                    session.rollback()
 
 except Exception as e:
     print(f"An error occurred: {e}")
