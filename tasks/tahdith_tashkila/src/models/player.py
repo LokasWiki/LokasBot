2aa5f473a7f8feae0475d3553cb8f403b1381856
lokas <lokashafeek7755@gmail.com>
2023-10-01 23:31:14 +0300

    add Player model

---
+++ tasks/tahdith_tashkila/src/models/player.py
@@ -0,0 +1,45 @@
+class Player:
+    _name = None
+    _number = None
+    _is_manager = None
+    _title = None
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def number(self) -> int:
+        return self._number
+
+    @property
+    def is_manger(self) -> bool:
+        return self._is_manager
+
+    @property
+    def title(self) -> str:
+        return self.title
+
+    @property
+    def has_number(self) -> bool:
+        return bool(self._number)
+
+    @property
+    def has_title(self) -> bool:
+        return bool(self._title)
+
+    @name.setter
+    def name(self, value):
+        self._name = str(value)
+
+    @number.setter
+    def number(self, value):
+        self._number = int(value)
+
+    @is_manger.setter
+    def is_manager(self, value):
+        self._is_manager = bool(value)
+
+    @title.setter
+    def title(self, value):
+        self._title = value
