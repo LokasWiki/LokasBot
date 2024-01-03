b2177f26d81576fb402b1aec8a09c0f21a534c36
lokas <lokashafeek7755@gmail.com>
2024-01-03 20:09:52 +0200

    add sort to list

--- tasks/statistics/wikidata/current_events_recent_obituaries.py
+++ tasks/statistics/wikidata/current_events_recent_obituaries.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import pywikibot
 from pywikibot import pagegenerators
 
@@ -22,6 +24,7 @@ LIMIT 20
 |row_template=بوابة:أحداث جارية/شريط جانبي/وفيات حديثة/قالب
 |page=بوابة:أحداث جارية/شريط جانبي/وفيات حديثة
 }}"""
+#  main query
 query = '''
 SELECT ?item
 WHERE { values ?offset {"1920-02-00T00:00:00"^^xsd:dateTime }. values ?offset0 { "1920-01-00T00:00:00"^^xsd:dateTime }.
@@ -34,20 +37,43 @@ WHERE { values ?offset {"1920-02-00T00:00:00"^^xsd:dateTime }. values ?offset0 {
 ORDER BY DESC(?dod)
 LIMIT 20
 '''
+#  connect to wikidata
 repo = wiki_site.data_repository()
 
 generator = pagegenerators.PreloadingEntityGenerator(pagegenerators.WikidataSPARQLPageGenerator(query, site=repo))
 
+#  start add item to list
 list_of_items = []
 
 for item in generator:
-    list_of_items.append(item.id)
+    #  we add unix timestamp to make sort desc cos order by of query not work
+    # todo: find better a way to make sort
+    try:
+        date_of_death = item.claims.get('P570')  # Assuming 'P570' is the property for date of death
+        if date_of_death:
+            date_string = date_of_death[0].getTarget()
+            unix_timestamp_string = f"{date_string.year}-{date_string.month}-{date_string.day} 00:00:00"
+            unix_timestamp = datetime.strptime(unix_timestamp_string, "%Y-%m-%d %H:%M:%S").timestamp()
+            list_of_items.append((item.id, unix_timestamp))
+        else:
+            print("No date of death found for", item.id)
+    except Exception as e:
+        print(e)
+#  sort item desc
+sorted_items_desc = sorted(list_of_items, key=lambda x: x[1], reverse=False)
+#  show only fisrt 20 items
+index = 0
+for row in sorted_items_desc:
+    #  to add only 20
+    if index == 20:
+        break
+    index += 1
+    id = row[0]
 
-for id in list_of_items:
     wiki_page_text += "\n"
     wiki_page_text += """ {{بوابة:أحداث جارية/شريط جانبي/وفيات حديثة/قالب | qid =  WIKI_ITEM }} """.replace("WIKI_ITEM",
                                                                                                             id)
-
+# start save page
 wiki_page_text += """
 {{Wikidata list end}}
 <noinclude>
@@ -59,4 +85,4 @@ wiki_page_text += """
 page_obj = pywikibot.Page(wiki_site, wiki_page_title)
 page_obj.text = wiki_page_text
 
-page_obj.save(summary="Wikidata list updated")
+page_obj.save(summary="Wikidata list updated (v0.0.2)")
