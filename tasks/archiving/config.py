9447fd9a61936707c4236e6d4285d13bc54a9e42
lokas <lokashafeek7755@gmail.com>
2024-08-03 11:19:31 +0300

    add USER_CONFIG

---
+++ tasks/archiving/config.py
@@ -0,0 +1,8 @@
+from typing import Dict
+
+USER_CONFIG: Dict[str, str] = {
+    'automated_archiving_template' :'أرشفة آلية',
+    'setion_type_name':'قسم',
+    'setion_type_size':'حجم',
+    'skip_template':'رشف'
+}
