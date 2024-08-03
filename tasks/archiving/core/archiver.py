a54c55872d19b2860e5b47725c828221de53f96d
lokas <lokashafeek7755@gmail.com>
2024-08-03 17:55:52 +0300

    Options

--- tasks/archiving/core/archiver.py
+++ tasks/archiving/core/archiver.py
@@ -4,6 +4,57 @@ import wikitextparser as wtp
 import pywikibot
 import hashlib
 
+
+class Options:
+    def __init__(self, page: pywikibot.Page, template_name: str = "أرشفة آلية"):
+        """
+        Initializes the object with the given `page` and `template_name`.
+
+        Parameters:
+            page (pywikibot.Page): The page object.
+            template_name (str, optional): The name of the template. Defaults to "أرشفة آلية".
+
+        Returns:
+            None
+        """
+        self.template_name = template_name
+        self.page = page
+        self.option = ('قسم', '3', None)
+        self._get_params()
+
+    def _get_template(self):
+        """
+        Retrieves the template with the specified name from the page's wikitext.
+
+        Returns:
+            wtp.Template or None: The template object if found, None otherwise.
+        """
+        text = self.page.get()
+        templates = wtp.parse(text).templates
+        for t in templates:
+            if t.name == self.template_name:
+                return t
+        return None
+
+    def _get_params(self):
+        """
+        Retrieves the parameters from the template.
+
+        Returns:
+            tuple or None: A tuple containing the values of the template arguments if the template has exactly three arguments,
+                           or None if the template is not found or has a different number of arguments.
+        """
+        template = self._get_template()
+        if template is None:
+            return None
+
+        arguments = template.arguments
+        if len(arguments) == 3:
+            self.option = (arguments[0].value, arguments[1].value, arguments[2].value)
+
+
+
+
 class Section:
     def __init__(self, title, content):
         self.title = title.strip()
@@ -24,6 +75,8 @@ class Archiver:
         """
         # The page to be edited
         self.talk_page = page
+        self.options = (Options(self.talk_page)).option
+
     def archive_talk_page(self, ARCHIVE_THRESHOLD_DAYS=3):
         """
         Archives the talk page of the user.
@@ -60,9 +113,8 @@ class Archiver:
         else:
             print("No sections to archive.")
 
-
     def get_last_comment_timestamps(self):
-        history = self.talk_page.revisions(reverse=True, total=500,content=True)  # Fetch last 500 revisions
+        history = self.talk_page.revisions(reverse=True, total=500, content=True)  # Fetch last 500 revisions
         section_last_edit = {}
         seen_sections = set()
 
@@ -90,11 +142,13 @@ class Archiver:
             seen_sections = current_sections
 
         return section_last_edit
+
     def _split_sections(self, text):
         parsed = wtp.parse(text)
         sections = parsed.sections
         # show only sections with level 2
         return [(section.title, section.string) for section in sections if section.level == 2]
+
     def _extract_header(self, text):
         parsed = wtp.parse(text)
         templates = parsed.templates
@@ -105,10 +159,9 @@ class Archiver:
                 headers.append(template.span[0])
                 headers.append(template.span[1])
         if len(headers) <= 1:
-            return  ""
+            return ""
         if len(headers) >= 2:
             return text[headers[0]:headers[-1]]
-
 
 
 site = pywikibot.Site('ar', 'wikipedia')
