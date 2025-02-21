7fb7b6b584218543d5e808796ade862cfd1e8c3d
lokas <lokashafeek7755@gmail.com>
2025-02-21 10:23:56 +0200

    Uncomment request processing scripts and update job configuration

--- toolforge/jobs/requests_read.sh
+++ toolforge/jobs/requests_read.sh
@@ -7,14 +7,14 @@ export PYWIKIBOT_DIR="$HOME/repos"
 export PYTHONPATH="${PYTHONPATH}:$HOME/repos"
 
 
-# python3  "$HOME"/repos/tasks/requests/link_replacement/read.py
-# python3  "$HOME"/repos/tasks/requests/template_distribution/read.py
-# python3  "$HOME"/repos/tasks/requests/add_category/read.py
-# python3  "$HOME"/repos/tasks/requests/portal_distribution/read.py
-# python3  "$HOME"/repos/tasks/requests/replace_template/read.py
-# python3  "$HOME"/repos/tasks/requests/cite/read.py
-# python3  "$HOME"/repos/tasks/requests/remove/read.py
-#python3  "$HOME"/repos/tasks/check_usernames/check/check.py
+python3  "$HOME"/repos/tasks/requests/link_replacement/read.py
+python3  "$HOME"/repos/tasks/requests/template_distribution/read.py
+python3  "$HOME"/repos/tasks/requests/add_category/read.py
+python3  "$HOME"/repos/tasks/requests/portal_distribution/read.py
+python3  "$HOME"/repos/tasks/requests/replace_template/read.py
+python3  "$HOME"/repos/tasks/requests/cite/read.py
+python3  "$HOME"/repos/tasks/requests/remove/read.py
+# python3  "$HOME"/repos/tasks/check_usernames/check/check.py
 python3  "$HOME"/repos/tasks/statistics/auto_update/cite_q.py
 
 # Exit the script after running all the Python files
