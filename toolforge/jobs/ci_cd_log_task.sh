f275ddadd0760c5aad632bb2a216bf030db39c5b
loka shafeek <lokashafeek7755@gmail.com>
2025-08-23 20:27:21 +0300

    fix name

--- toolforge/jobs/ci_cd_log_task.sh
+++ toolforge/jobs/ci_cd_log_task.sh
@@ -8,7 +8,7 @@ export PYWIKIBOT_DIR="$HOME/repos"
 export PYTHONPATH="${PYTHONPATH}:$HOME/repos"
 
 
-python3  "$HOME"/repos/tasks/ci_cd_log_task/bot.py
+python3  "$HOME"/repos/tasks/ci_cd_log_task/main.py
 
 # Exit the script after running all the Python files
 exit 0
